8cbccedc16ef9f4b3ed10a1613d5d5f6065e5097
rodro7 <rodrigo.rossi.trabal@gmail.com>
2016-08-29 22:53:07 -0300

    add tenant-id header to interceptor filter

--- lcbsapi/src/main/java/lcbs/api/interceptor/TenantCheckInterceptor.java
+++ lcbsapi/src/main/java/lcbs/api/interceptor/TenantCheckInterceptor.java
@@ -14,9 +14,15 @@ public class TenantCheckInterceptor implements javax.ws.rs.container.ContainerRe
 	TenantRepo repo; 
     @Override
     public void filter(ContainerRequestContext requestContext){
+    	 String id = requestContext.getHeaderString("lcbs-tenant");
     	 String host =  requestContext.getHeaderString("host");
 		 DataTenant filter = new DataTenant();
-		 filter.setDomain(host); 
+		 if(id != null && !id.isEmpty()){
+			 filter.setId(id);
+		 }else{
+			 filter.setDomain(host); 	 
+		 }
+		 
 		 DataTenant tenant = repo.get(filter);
 		 requestContext.setProperty("tenant", tenant);
 		 //TODO: abort here if not tenant is available for information provided in the header.

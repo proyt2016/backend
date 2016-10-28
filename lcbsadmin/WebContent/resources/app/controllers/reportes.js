60f07fa041e5a2fdad023013ef3fbbab4e915953
Maxis <maxis6791@gmail.com>
2016-10-28 00:15:05 -0300

    total reporte diario pasajes/encomiendas

--- lcbsadmin/WebContent/resources/app/controllers/reportes.js
+++ lcbsadmin/WebContent/resources/app/controllers/reportes.js
@@ -9,25 +9,43 @@
     	
         $scope.pasajes    = [];
         $scope.encomiendas = [];
+        $scope.totalPasaje = 0;
+        $scope.totalEnco = 0;
         $scope.showAlert    = false;
 
         console.log($routeParams);
 
-       var initialize  = function(){
+        var initialize  = function(){
            var now = moment();
+           var total = 0;
            var dateNow =  moment(now, 'DD/MM/YYYY').format('YYYY-MM-DD');
         	reportesService.getPasajesVendidos(dateNow).then(function (data) {
                     $scope.pasajes = data;
-                });          
+                        for(var i = 0; i < $scope.pasajes.length; i++){
+                            total  = total + $scope.pasajes[i].precio.monto;
+                            $scope.totalPasaje = total;
+                            console.log(total);  
+                        }   
+            });       
+
         }
 
-       var initializeEnco  = function(){
+        var initializeEnco  = function(){
+           var totalEn = 0;
            var now = moment();
            var dateNow =  moment(now, 'DD/MM/YYYY').format('YYYY-MM-DD');
            reportesService.getEncomiendasPagas(dateNow).then(function (data){
                     $scope.encomiendas = data;
+                    for(var i = 0; i < $scope.encomiendas.length; i++){
+                            totalEn  = totalEn + $scope.encomiendas[i].monto;
+                            $scope.totalEnco = totalEn;
+                            console.log(total);  
+                        }   
             }); 
         }
+
+     
+
         initialize();
         initializeEnco();
     }

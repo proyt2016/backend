0dab59c6e7bdd1fd910c12cc57b9beb76865b73b
rodro7 <rodrigo.rossi.trabal@gmail.com>
2016-11-28 21:26:42 -0300

    change tenant validation

--- lcbsapi/src/main/java/lcbs/api/interceptor/TenantCheckInterceptor.java
+++ lcbsapi/src/main/java/lcbs/api/interceptor/TenantCheckInterceptor.java
@@ -8,35 +8,44 @@ import javax.ws.rs.ext.Provider;
 import lcbs.api.service.TenantRepo;
 import lcbs.exceptions.TenantException;
 import lcbs.shares.DataTenant;
+
 @Provider
-@TenantChecked 
+@TenantChecked
 public class TenantCheckInterceptor implements javax.ws.rs.container.ContainerRequestFilter {
-	
+
 	@EJB
-	TenantRepo repo; 
-    @Override
-    public void filter(ContainerRequestContext requestContext){
-     
-    	 String id = requestContext.getHeaderString("lcbs-tenant"); 
-    	 String host =  requestContext.getHeaderString("host");
-		 DataTenant filter = new DataTenant();
-		 if(id != null && !id.isEmpty()){
-			 filter.setId(id);
-		 }else{
-			 filter.setDomain(host); 	 
-		 }
-		 filter.setIsActive(true);
-		 filter.setIsDelete(false);
-		 try{
-			 DataTenant tenant = repo.get(filter);
-			 if(tenant == null){
-				 throw new ForbiddenException("Servicio no disponible");
-			 }else{
-				 requestContext.setProperty("tenant", tenant);
-			 }
-		 }catch(TenantException e){
-			 throw new ForbiddenException("No permitido");
-		 } 
-    } 
+	TenantRepo repo;
+	String rs;
+
+	@Override
+	public void filter(ContainerRequestContext requestContext) {
+
+		String id = requestContext.getHeaderString("lcbs-tenant");
+		// String host = requestContext.getHeaderString("host");
+		String referer = requestContext.getHeaderString("Referer");
+		String[] sp = referer.replaceAll("http://", "").split("/");
+		String host = sp.length > 0 ? sp[0] : null;
+		requestContext.getHeaders().keySet().forEach((key) -> {
+			rs += key + ":" + requestContext.getHeaders().get(key) + "/";
+		});
+		DataTenant filter = new DataTenant();
+		if (id != null && !id.isEmpty()) {
+			filter.setId(id);
+		} else {
+			filter.setDomain(host);
+		}
+		filter.setIsActive(true);
+		filter.setIsDelete(false);
+		try {
+			DataTenant tenant = repo.get(filter);
+			if (tenant == null) {
+				throw new ForbiddenException("Servicio no disponible: " + host + " / " + rs);
+			} else {
+				requestContext.setProperty("tenant", tenant);
+			}
+		} catch (TenantException e) {
+			throw new ForbiddenException("No permitido:" + rs);
+		}
+	}
 
 }
